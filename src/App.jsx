4d9071dcc8e8d102d935786f3e9baa190ea0aa14
Hamza khan <hamzakhan.std25@gmail.com>
2025-08-10 13:35:17 +0500

    error fixes while delpolyment on vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './styles/global.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Footer from './components/footer'
 import Home from './pages/Home'     
 import Internship from './pages/Internship'
